d58e14f74d3eb7cab80de2a648071898e1f3b6a4
Tomasz Miskiewicz <tmiskiewicz.159@gmail.com>
2018-02-05 20:02:49 +0100

    Commit message

--- inventory_interfejs.py
+++ inventory_interfejs.py
@@ -9,29 +9,29 @@ class TextInterface:
     print(f'\nThere is no wallet, watches, cufflinks, ties, bow ties')
     
     while True:
-        komenda = input('What would like you to do ? add stuff(s), add price(p)' + \
+        command = input('What would like you to do ? add stuff(s), add price(p)' + \
                         ' estimate the value of product(e), show stuff(f), end program(x) ')
         print()
         
-        if komenda == 's':
+        if command == 's':
             stuff = input('What product do you want to add to the inventory ? ')
             quantity = int(input('How many product do you want to add to the inventory ? '))
             inventory_program.add_stuff(stuff=stuff, quantity= quantity)
-        elif komenda == 'p':
+        elif command == 'p':
             stuff = input('What product do you want price ? ')
             price = int(input('Please, enter the price of product: '))
             inventory_program.add_price(stuff=stuff, price=price)
-        elif komenda == 'e':
+        elif command == 'e':
             stuff = input('The value of what product do you want to estimate ? ')
             quantity = int(input('How many products do you want to price ? '))
             equal = inventory_program.value_stuff(stuff=stuff, quantity=quantity)
             print()
             print(f'The value equal: {equal}')
-        elif komenda == 'f':
+        elif command == 'f':
             print(inventory_program.generation_table())
-        elif komenda == 'x':
+        elif command == 'x':
             print(f'Thank you for using "{inventory_program.name}"')
             break
-        elif komenda != 's' or komenda != 'p':
-            print(f'It is a wrong reference {komenda}')
+        elif command != 's' or command != 'p':
+            print(f'It is a wrong reference {command}')
     
